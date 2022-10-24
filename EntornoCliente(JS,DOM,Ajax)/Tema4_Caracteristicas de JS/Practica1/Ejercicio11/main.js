82fc761213db43dbc931f081cc2ed559af837696
Andrei Miu <ftp.andreimiu@gmail.com>
2022-10-24 14:01:08 +0200

    a medias

--- EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio11/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio11/main.js
@@ -1,10 +1,28 @@
 "use strict";
 function numNatural() {
   let numero = parseInt(document.getElementById("numNatural").value);
-  alert(calculoNumNatural(numero));
+  alert("El numero " + numero + " se divide en: " + calculoNumNatural(numero));
 }
 
-function calculoNumNatural(numero) {}
+function calculoNumNatural(numero) {
+  let arrNum = [];
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < numero; j++) {
+      if (j * j > numero) {
+        arrNum[i] = j - 1;
+        numero = numero - (j - 1) * (j - 1);
+        break;
+      } else {
+        if (j * j == numero) {
+          arrNum[i] = j;
+          numero = numero - j * j;
+          break;
+        }
+      }
+    }
+  }
+  return arrNum;
+}
 
 function soloNumeros(num) {
   var code = num ? num.which : num.keyCode;
