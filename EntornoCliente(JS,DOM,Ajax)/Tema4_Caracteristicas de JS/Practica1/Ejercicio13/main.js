dda1e87bd1dfce9a803dfa931602d3396650a617
Andrei Miu <ftp.andreimiu@gmail.com>
2022-10-25 14:22:56 +0200

    13 a medias

--- EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
@@ -1,15 +1,30 @@
 "use strict";
-function alerta() {
-  let cadena = document.getElementById("searchInput").value.toLowerCase();
-  alert(buscaVocales(cadena));
+function formarTriangulo() {
+  let cadena = document
+    .getElementById("searchInput")
+    .value.toUpperCase()
+    .split("");
+  alert(triangulo(cadena));
 }
 
-function buscaVocales(cadena) {
-  let suma = 0;
-  for (let i = 0; i < cadena.length; i++) {
-    if (cadena.charAt(i).value("aeiou")) {
-      suma++;
+function triangulo(cadena) {
+  let arrCadena = [];
+  let arrBG = ["B", "G"];
+  let arrRG = ["R", "G"];
+  for (let contador = 0; contador < cadena.length; contador++) {
+    for (let i = 0; i < cadena.length; i++) {
+      if (cadena[i] == cadena[i + 1]) {
+        arrCadena[i] = cadena[i];
+      } else if (cadena.indexOf(i, i + 1) == arrBG) {
+        arrCadena[i] == "R";
+      } else if (cadena.indexOf(i, i + 1) == arrRG) {
+        arrCadena[i] == "B";
+      } else {
+        arrCadena[i] == "G";
+      }
     }
+    cadena = arrCadena;
   }
-  return suma;
+
+  return cadena;
 }
