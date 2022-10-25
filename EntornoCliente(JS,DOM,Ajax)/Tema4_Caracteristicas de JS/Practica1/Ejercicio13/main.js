2a31f26e9a356e93ed2643a34ed124dfb1702d8b
Andrei <ftp.andreimiu@gmail.com>
2022-10-25 17:46:34 +0200

    ej13

--- EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
@@ -11,8 +11,8 @@ function triangulo(cadena) {
   let arrCadena = [];
   let arrBG = ["B", "G"];
   let arrRG = ["R", "G"];
-  for (let contador = cadena; contador > 0; contador--) {
-    for (let i = 0; i < cadena.length; i++) {
+  for (let contador = 0; contador < cadena.length; contador++) {
+    for (let i = 0; i < cadena.length - 1; i++) {
       if (cadena[i] == cadena[i + 1]) {
         arrCadena[i] = cadena[i];
       } else if (arrBG.includes(cadena[i]) && arrBG.includes(cadena[i + 1])) {
