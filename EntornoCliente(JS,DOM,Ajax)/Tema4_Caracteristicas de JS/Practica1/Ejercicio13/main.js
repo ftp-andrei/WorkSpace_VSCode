f007cb249aaf17e1e58081d9b5d4388cae75f9ea
Andrei <ftp.andreimiu@gmail.com>
2022-10-25 17:11:39 +0200

    ej7 realizado

--- EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio13/main.js
@@ -11,16 +11,16 @@ function triangulo(cadena) {
   let arrCadena = [];
   let arrBG = ["B", "G"];
   let arrRG = ["R", "G"];
-  for (let contador = 0; contador < cadena.length; contador++) {
+  for (let contador = cadena; contador > 0; contador--) {
     for (let i = 0; i < cadena.length; i++) {
       if (cadena[i] == cadena[i + 1]) {
         arrCadena[i] = cadena[i];
-      } else if (cadena.indexOf(i, i + 1) == arrBG) {
-        arrCadena[i] == "R";
-      } else if (cadena.indexOf(i, i + 1) == arrRG) {
-        arrCadena[i] == "B";
+      } else if (arrBG.includes(cadena[i]) && arrBG.includes(cadena[i + 1])) {
+        arrCadena[i] = "R";
+      } else if (arrRG.includes(cadena[i]) && arrRG.includes(cadena[i + 1])) {
+        arrCadena[i] = "B";
       } else {
-        arrCadena[i] == "G";
+        arrCadena[i] = "G";
       }
     }
     cadena = arrCadena;
--- EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio7/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema4_Caracteristicas de JS/Practica1/Ejercicio7/main.js
@@ -6,7 +6,19 @@ function alerta() {
 
 function indiceN(numero) {
   numero = numero.split(",");
-  alert(numero);
-
-  for (let i = 0; i < numero.length() / 2; i++) {}
+  let sumaIzq = 0;
+  let sumaDer = 0;
+  for (let i = 0; i < numero.length; i++) {
+    if (i != 0) {
+      sumaIzq = sumaIzq + parseInt(numero[i - 1]);
+    }
+    for (let j = i + 1; j < numero.length; j++) {
+      sumaDer = sumaDer + parseInt(numero[j]);
+    }
+    if (sumaIzq == sumaDer) {
+      return i;
+    }
+    sumaDer = 0;
+  }
+  return -1;
 }
