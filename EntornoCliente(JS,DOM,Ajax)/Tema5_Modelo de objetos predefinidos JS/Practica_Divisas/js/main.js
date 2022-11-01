52d794ee0a288434ec2b5b751a2672b1493b3712
Andrei Miu <ftp.andreimiu@gmail.com>
2022-11-01 15:02:53 +0100

    añadida funcion exchange

--- EntornoCliente(JS,DOM,Ajax)/Tema5_Modelo de objetos predefinidos JS/Practica_Divisas/js/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema5_Modelo de objetos predefinidos JS/Practica_Divisas/js/main.js
@@ -1,3 +1,21 @@
+function cambiar() {
+  let importe = document.getElementById("importe").value;
+  let moneda1 = document.getElementById("moneda1").value.toLowerCase();
+  let moneda2 = document.getElementById("moneda2").value.toLowerCase();
+
+  // 1 Euros = Dollars 1.05
+  // 1 Libra  = Dollars  1.30
+}
+
+function exchange() {
+  let moneda1 = document.getElementById("moneda1").value;
+  let moneda2 = document.getElementById("moneda2").value;
+  if (moneda1 != moneda2) {
+    document.getElementById("moneda2").value = moneda1;
+    document.getElementById("moneda1").value = moneda2;
+  }
+}
+
 function soloNumeros(num) {
   var code = num ? num.which : num.keyCode;
   if (code == 8) {
