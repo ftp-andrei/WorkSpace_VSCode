077e15d2183c65cf7abdcbf4345e491b6e1d85d1
Andrei Miu <ftp.andreimiu@gmail.com>
2022-11-13 11:06:48 +0200

    quitar comentarios

--- EntornoCliente(JS,DOM,Ajax)/Tema5_Modelo de objetos predefinidos JS/Practica_Divisas/js/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema5_Modelo de objetos predefinidos JS/Practica_Divisas/js/main.js
@@ -2,8 +2,6 @@ function cambiar() {
   let importe = document.getElementById("importe").value;
   let moneda1 = document.getElementById("moneda1").value.toLowerCase();
   let moneda2 = document.getElementById("moneda2").value.toLowerCase();
-  // 1 Euros = Dollars 1.05
-  // 1 Libra  = Dollars  1.30
 
   let resultado = 0;
   if (importe != 0) {
@@ -56,13 +54,10 @@ function exchange() {
 function soloNumeros(num) {
   var code = num ? num.which : num.keyCode;
   if (code == 8) {
-    // backspace.
     return true;
   } else if (code >= 48 && code <= 57) {
-    // is a number.
     return true;
   } else {
-    // other keys.
     return false;
   }
 }
