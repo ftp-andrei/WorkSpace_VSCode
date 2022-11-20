d59cacf1b628965426b33e44605222e7a15fe3b9
Andrei Miu <ftp.andreimiu@gmail.com>
2022-11-20 02:28:36 +0100

    libreriaComercio v2.1

--- EntornoCliente(JS,DOM,Ajax)/Tema6_JSON y Objetos JS/Practica_LibreriaComercio/js/main.js
+++ EntornoCliente(JS,DOM,Ajax)/Tema6_JSON y Objetos JS/Practica_LibreriaComercio/js/main.js
@@ -2,10 +2,10 @@
 
 Usar Local.storage
 
-localStorage.setItem('nombre', 'Juan');
-localStorage.getItem('nombre');
-localStorage.removeItem('nombre');
-localStorage.clear();
+sessionStorage.setItem('nombre', 'Juan');
+sessionStorage.getItem('nombre');
+sessionStorage.removeItem('nombre');
+sessionStorage.clear();
 
 */
 "use strict";
@@ -33,38 +33,76 @@ function crearLibro() {
   let titulo = document.getElementById("titulo").value;
   let nombre = document.getElementById("nombre").value;
   let paginas = document.getElementById("paginas").value;
-  let prestado = document.getElementById("checkboxPrestado").checked;
+  let prestado = "";
+  if (document.getElementById("checkboxPrestado").checked) {
+    prestado = "Si";
+  } else {
+    prestado = "No";
+  }
   let nuevoLibro = null;
 
   nuevoLibro = new Libro(titulo, nombre, paginas, prestado);
 
-  localStorage.setItem(localStorage.length, JSON.stringify(nuevoLibro));
+  sessionStorage.setItem(sessionStorage.length, JSON.stringify(nuevoLibro));
 }
 
 function recuperarDatosArray() {
-  let len = localStorage.length;
+  let len = sessionStorage.length;
   for (let i = 0; i < len; i++) {
-    arrayLibros[i] = localStorage[i];
+    arrayLibros[i] = sessionStorage[i];
   }
   return arrayLibros;
 }
 
 function mostrarLibros() {
   let arr = recuperarDatosArray();
-  // recuperamos el arrLibros
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     const contenedor = document.getElementById("wrapMain");
     const tabla = document.createElement("table");
     let tr = document.createElement("tr");
     let td = document.createElement("td");
     let th = document.createElement("th");
-
-    let trTxt = document.createTextNode(JSON.parse(arr[i]).titulo);
-    let tdTxt;
-    th.appendChild(trTxt);
+    let br = document.createElement("br");
+    let trTituloTabla = document.createTextNode(JSON.parse(arr[i]).titulo);
+    th.appendChild(trTituloTabla);
     tr.appendChild(th);
+
     tabla.appendChild(tr);
+
+    tr = document.createElement("tr");
+
+    td = document.createElement("td");
+    let tdNombreTabla = document.createTextNode(
+      "Nombre Autor: " + JSON.parse(arr[i]).nombre
+    );
+    td.appendChild(tdNombreTabla);
+    tr.appendChild(td);
+
+    tabla.appendChild(tr);
+
+    tr = document.createElement("tr");
+
+    td = document.createElement("td");
+    let tdPaginasTabla = document.createTextNode(
+      "Paginas: " + JSON.parse(arr[i]).paginas
+    );
+    td.appendChild(tdPaginasTabla);
+    tr.appendChild(td);
+
+    tabla.appendChild(tr);
+
+    tr = document.createElement("tr");
+
+    td = document.createElement("td");
+    let tdPrestadoTabla = document.createTextNode(
+      "Prestado: " + JSON.parse(arr[i]).prestado
+    );
+    td.appendChild(tdPrestadoTabla);
+    tr.appendChild(td);
+
+    tabla.appendChild(tr);
+
     contenedor.appendChild(tabla);
   }
 }
