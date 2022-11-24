06f97bdf3e2c38eda3153b9e17ce7d5f3c74534b
Andrei <ftp.andreimiu@gmail.com>
2022-11-24 18:42:54 +0100

    pdo

--- EntornoServidor(PHP)/BaseDeDatos/PDOConsulta/select.php
+++ EntornoServidor(PHP)/BaseDeDatos/PDOConsulta/select.php
@@ -6,5 +6,6 @@ $con = new PDO('sqlsrv:server=(local);database=AdventureWorks2019', "", "");
 $stn = $con->prepare($sql);
 $stn->execute();
 $files = $stn->fetchAll(PDO::FETCH_ASSOC);
+$con = null;
 var_dump($files);
 // Es asociativa al select no a la bbdd/tabla
