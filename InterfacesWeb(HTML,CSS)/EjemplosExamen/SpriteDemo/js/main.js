5db8a1067d974f828e2adf4e9859180d3d47afdc
Andrei Miu <ftp.andreimiu@gmail.com>
2022-12-16 17:24:55 +0100

    ejemplos animacion

---
+++ InterfacesWeb(HTML,CSS)/EjemplosExamen/SpriteDemo/js/main.js
@@ -0,0 +1,14 @@
+// "use strict";
+
+// import "../css/general.css";
+
+// let step = 1;
+// const movement = 18;
+// const maxStep = 8;
+
+// setInterval(() => {
+//   const $sprite = document.querySelector(".sprite");
+//   const x = step * movement;
+//   step = step > maxStep ? 1 : step + 1;
+//   $sprite.style.backgroundPosition = `-${x}px 0px`;
+// }, 100);
