79040f7e9211f427d03eccb059c4b45b817131df
Andrei <ftp.andreimiu@gmail.com>
2023-02-14 18:17:51 +0100

    php

---
+++ EntornoServidor(PHP)/Iberia - copia/app/plantillas/inicio.php
@@ -0,0 +1,90 @@
+<?php
+/* Ejemplo de plantilla que se mostrará dentro de la plantilla principal
+  ob_start() activa el almacenamiento en buffer de la página. Mientras se
+             almacena en el buffer no se produce salida alguna hacia el
+             navegador del cliente
+  luego viene el código html y/o php que especifica lo que debe aparecer en
+     el cliente web
+  ob_get_clean() obtiene el contenido del buffer (que se pasa a la variable
+             $contenido) y elimina el contenido del buffer
+  Por último se incluye la página que muestra la imagen común de la aplicación
+    (en este caso base.php) la cual contiene una referencia a la variable
+    $contenido que provocará que se muestre la salida del buffer dentro dicha
+    página "base.php"
+*/
+?>
+<?php ob_start();
+$destinos = $_SESSION['destinos'];
+?>
+
+<form action="" method="post">
+  <fieldset>
+    <legend>Buscador de vuelos</legend>
+    <label for="salida">Salir de: <input list="listaSalida" name="salida" id="salida"></label>
+    <datalist id="listaSalida">
+      <?php foreach ($destinos as $key) {
+        echo "<option value='$key'>";
+      } ?>
+    </datalist>
+    <label for="destino">Destino: <input list="listaDestino" name="destino" id="destino"></label>
+    <datalist id="listaDestino">
+      <?php
+      foreach ($destinos as $key) {
+        echo "<option value='$key'>";
+      }
+      ?>
+    </datalist>
+    <label for="ida">Ida: <input type="date" name="ida" id="ida"></label>
+    <label for="vuelta">Vuelta: <input type="date" name="vuelta" id="vuelta"></label>
+    <input type="submit" value="Buscar" name="ok" id="ok">
+  </fieldset>
+
+</form>
+<?php
+if (isset($vuelos)) { ?>
+  <table>
+    <thead>
+      <caption>Resultado</caption>
+      <th>&nbsp;</th>
+      <th>Código</th>
+      <th>Origen</th>
+      <th>Destino</th>
+      <th>Salida</th>
+      <th>Duración</th>
+      <th>Fecha</th>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Ida:</td>
+        <?php foreach ($vuelos as $ida => $value) {
+          if ($ida === 'ida') {
+            foreach ($value as $arrayIda => $value2) {
+              foreach ($value2 as $valoresIda => $valor) {
+        ?>
+                <td><?php echo $valor;  ?></td>
+        <?php  }
+            }
+          }
+        } ?>
+      </tr>
+      <tr>
+        <td>Vuelta:</td>
+        <?php foreach ($vuelos as $ida => $value) {
+          if ($ida === 'vuelta') {
+            foreach ($value as $arrayIda => $value2) {
+              foreach ($value2 as $valoresIda => $valor) {
+        ?>
+                <td><?php echo $valor;  ?></td>
+        <?php  }
+            }
+          }
+        } ?>
+      </tr>
+    </tbody>
+  </table>
+<?php } ?>
+
+<?php
+$contenido = ob_get_clean();
+include 'base.php'; ?>
---
+++ EntornoServidor(PHP)/Iberia - copia/app/plantillas/reservar.php
@@ -0,0 +1,75 @@
+<?php ob_start();
+$destinos = $_SESSION['destinos'];
+?>
+
+<form action="" method="post">
+    <fieldset>
+        <legend>Reservas de vuelos</legend>
+        <label for="salida">Salir de: <input list="listaSalida" name="salida" id="salida" required></label>
+        <datalist id="listaSalida">
+            <?php foreach ($destinos as $key) {
+                echo "<option value='$key'>";
+            } ?>
+        </datalist>
+        <label for="destino">Destino: <input list="listaDestino" name="destino" id="destino" required></label>
+        <datalist id="listaDestino">
+            <?php
+            foreach ($destinos as $key) {
+                echo "<option value='$key'>";
+            }
+            ?>
+        </datalist>
+        <label for="ida">Ida: <input type="date" name="ida" id="ida" required>
+        </label>
+        <label for="vuelta">Vuelta: <input type="date" name="vuelta" id="vuelta" required> </label>
+        <input type="submit" value="Buscar" name="ok" id="ok">
+    </fieldset>
+
+</form>
+<?php if (isset($vuelos)) { ?>
+    <table>
+        <thead>
+            <tr>
+                <th>A</th>
+                <th>B</th>
+                <th>C</th>
+                <th>Fila</th>
+                <th>D</th>
+                <th>E</th>
+                <th>F</th>
+            </tr>
+        </thead>
+        <!--  Quitar dias en las fechas -->
+        <tbody>
+            <?php foreach ($vuelos as $ida => $value) {
+                if ($ida === 'ida') { ?>
+                    <caption><?php echo $value[0]["Código"] . " " . $value[0]["Origen"] . "-" . $value[0]["Destino"] . " " . $value[0]["fecha"] ?></caption>
+            <?php
+                    $fila = 1;
+
+                    $asientosVuelo = [];
+                    for ($i = 1; $i <= 20; $i++) {
+                        $asientosVuelo[$i] = (int)$value[0]["fila" . $i];
+                    }
+                    for ($x = 1; $x <= 20; $x++) {
+                        echo "<tr>";
+                        echo "<td " . ($asientosVuelo[$x] & 32 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "<td " . ($asientosVuelo[$x] & 16 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "<td " . ($asientosVuelo[$x] & 8 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "<td>" . $fila . "</td>";
+                        echo "<td " . ($asientosVuelo[$x] & 4 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "<td " . ($asientosVuelo[$x] & 2 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "<td " . ($asientosVuelo[$x] & 1 ? "class='ocupado'>Ocupado" : "class='libre'><input type='checkbox' name='libre'>") . "</td>";
+                        echo "</tr>";
+                        $fila++;
+                    }
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+<?php
+    echo "<div class='centrar'><input type='submit' value='Comprar' name='comprar' id='comprar'><div>";
+}
+$contenido = ob_get_clean();
+include 'base.php'; ?>
