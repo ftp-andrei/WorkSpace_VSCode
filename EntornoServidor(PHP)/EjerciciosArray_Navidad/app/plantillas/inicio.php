1b698faaa225864377099948cac11b21dd824e0e
Andrei <ftp.andreimiu@gmail.com>
2023-02-15 19:51:41 +0100

    ej1

--- EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/inicio.php
+++ EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/inicio.php
@@ -17,13 +17,44 @@ ob_start();
 <h1>Ejercicio 1</h1>
 <p>Realiza un guion PHP en el que se muestren los valores en euros de las antiguas denominaciones de los billetes en pesetas. Hubo billetes de 100, 200, 500, 1000, 2000, 5000 y 10000 pesetas. Recuerda que un euro son 166,386 pesetas. Utiliza un array PHP para realizar el ejercicio. Utiliza un bucle for para calcular el valor en euros y un bucle foreach para mostrar los resultados en una tabla HTML. Emplea CSS. Los resultados deben mostrarse con un máximo de 2 decimales. Busca en la documentación de PHP qué funciones puedes emplear para mejorar el proceso.</p>
 <form action="" method="post">
-  <label for="euros">Euros: <input type="text" name="euros" id="euros"></label> <input type="button" value="Cambiar" name="btnCambio" id="btnCambio"><br>
-  <label>Pesetas: <input type="text" name="pesetas" value="0" disabled></label> <br>
-</form>
-<?php
-if (isset($euros)) {
-  echo $euros;
-}
 
+  <fieldset id="fieldEj1">
+    <legend>Conversor pesetas</legend>
+    <label for="euros">Euros: <input type="text" name="euros" id="euros"></label>
+    <label>Pesetas: <input type="text" name="pesetas" value="
+  <?php
+  if (isset($euros)) {
+    echo $euros;
+  } else {
+    echo 0;
+  } ?>" disabled></label>
+    <br>
+    <input type="submit" value="Cambiar" name="btnCambio" id="btnCambio">
+  </fieldset>
+</form>
+
+<?php
+$arrPesetas = [100, 200, 500, 1000, 2000, 5000, 10000];
+?>
+<table>
+  <thead>
+    <tr>
+      <td>Euros</td>
+      <td>Pesetas</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <!-- 1 - 166
+      X - 100
+      100/166 
+    
+    Hacer bucle for-->
+    </tr>
+  </tbody>
+</table>
+<?php
 $contenido = ob_get_clean();
 include 'base.php'; ?>
