bb01af17d7e8d0d5e4789f7e719d0ec3ff75900e
Andrei <ftp.andreimiu@gmail.com>
2023-02-15 19:54:19 +0100

    ej1

--- EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/inicio.php
+++ EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/inicio.php
@@ -13,6 +13,7 @@
     página "base.php"
 */
 ob_start();
+$arrPesetas = [100, 200, 500, 1000, 2000, 5000, 10000];
 ?>
 <h1>Ejercicio 1</h1>
 <p>Realiza un guion PHP en el que se muestren los valores en euros de las antiguas denominaciones de los billetes en pesetas. Hubo billetes de 100, 200, 500, 1000, 2000, 5000 y 10000 pesetas. Recuerda que un euro son 166,386 pesetas. Utiliza un array PHP para realizar el ejercicio. Utiliza un bucle for para calcular el valor en euros y un bucle foreach para mostrar los resultados en una tabla HTML. Emplea CSS. Los resultados deben mostrarse con un máximo de 2 decimales. Busca en la documentación de PHP qué funciones puedes emplear para mejorar el proceso.</p>
@@ -33,9 +34,6 @@ ob_start();
   </fieldset>
 </form>
 
-<?php
-$arrPesetas = [100, 200, 500, 1000, 2000, 5000, 10000];
-?>
 <table>
   <thead>
     <tr>
