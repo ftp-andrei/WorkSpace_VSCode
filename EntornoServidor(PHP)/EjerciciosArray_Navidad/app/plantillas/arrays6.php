13d0c51a82436aeae0f65548066cf1030881b129
Andrei <ftp.andreimiu@gmail.com>
2023-02-16 20:05:28 +0100

    php

--- EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/arrays6.php
+++ EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/arrays6.php
@@ -16,11 +16,32 @@ ob_start();
 ?>
 <h2>Ejercicio 6</h2>
 <p>Completa el ejercicio anterior informando al final de las notas obtenidas: cantidad de Muy deficiente, Deficiente, Insuficiente, Suficiente, Bien, Notable y Sobresaliente obtenidos, y después la nota media obtenida y cuál sería su nota en formato cadena.</p>
-<form action="" method="post">
-
-
-</form>
-
-<?php
+<?php if ($notas) {
+  echo "Array notas: ";
+  print_r($notas);
+?>
+  <table>
+    <caption>Notas obtenidas</caption>
+    <thead>
+      <th>Nota</th>
+      <th>Cantidad</th>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($notaEscrita as $key => $nota) { ?>
+        <tr>
+          <td><?php echo $nota; ?></td>
+          <td><?php echo $cantidad[$nota]; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+      <tr>
+        <th>Media</th>
+        <td><?php echo $media ?></td>
+      </tr>
+    </tbody>
+  </table>
+<?php }
 $contenido = ob_get_clean();
 include 'base.php'; ?>
