94a307c621f9c836104157bf8360f70c1af61127
Andrei Miu <ftp.andreimiu@gmail.com>
2023-02-20 13:53:09 +0100

    php

--- EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/navidad2.php
+++ EntornoServidor(PHP)/EjerciciosArray_Navidad/app/plantillas/navidad2.php
@@ -8,15 +8,13 @@ ob_start();
   (Este caso es similar al anterior, pero aquí, tal vez debáis emplear funciones relacionadas con valores
   numéricos y/o de precisión) </p>
 <form action="" method="post">
-  <label for="numero1">Altura: <input type="number" name="numero1" id="numero1" value="0" required></label>
-  <label for="numero2">Diametro: <input type="number" name="numero2" id="numero2" value="0" required></label>
+  <label for="num1">Altura: <input type="number" name="num1" id="num1" value="0" required></label>
+  <label for="num2">Diametro: <input type="number" name="num2" id="num2" value="0" required></label>
   <input type="submit" value="Ok" name="ok">
 </form>
 <?php
 if (isset($_POST['ok'])) {
   echo $volumen;
 }
-?>
-<?php
 $contenido = ob_get_clean();
 include 'base.php'; ?>
