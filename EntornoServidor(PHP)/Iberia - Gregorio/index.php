5def356573cd0030c373a06195f764785c28322e
Andrei Miu <ftp.andreimiu@gmail.com>
2023-03-01 12:58:46 +0100

    wp

--- EntornoServidor(PHP)/Iberia - Gregorio/index.php
+++ EntornoServidor(PHP)/Iberia - Gregorio/index.php
@@ -2,15 +2,15 @@
 //index.php
 session_start();
 error_reporting(E_ALL);
-spl_autoload_register(function(string $clase){
-  if(strpos($clase,'app\\iberia\\')===0){
-    $nombre=str_replace('app\\iberia\\','',$clase);
-    $nombre=str_replace('\\','/',$nombre);
-    require_once __DIR__.'/fuente/'.$nombre.'.inc';
+spl_autoload_register(function (string $clase) {
+  if (strpos($clase, 'app\\iberia\\') === 0) {
+    $nombre = str_replace('app\\iberia\\', '', $clase);
+    $nombre = str_replace('\\', '/', $nombre);
+    require_once __DIR__ . '/fuente/' . $nombre . '.inc';
   }
 });
-if(!isset($_SESSION['usuario'])){
-  $_SESSION['usuario']='anonimo';
+if (!isset($_SESSION['usuario'])) {
+  $_SESSION['usuario'] = 'anonimo';
 }
 
 //require_once __DIR__ . '/fuente/Controlador/defaultController.inc'; /*controladores */
@@ -18,36 +18,35 @@ require_once __DIR__ . '/app/conf/rutas.inc'; /*Ubicación del archivo de rutas*
 
 
 // Parseo de la ruta
-if (isset($_GET['ctl']))
-{ 
-  if (isset($mapeoRutas[$_GET['ctl']]))
-  { 
+if (isset($_GET['ctl'])) {
+  if (isset($mapeoRutas[$_GET['ctl']])) {
     $ruta = $_GET['ctl'];
-    if(isset($_GET['vuelta'])){
-      $param=$_GET['vuelta'];
+    if (isset($_GET['vuelta'])) {
+      $param = $_GET['vuelta'];
     }
-  } else { 
+  } else {
     header('Status: 404 Not Found');
     echo '<html><body><h1>Error 404: No existe la ruta <i>' .
-          $_GET['ctl'] .
-          '</p></body></html>';
+      $_GET['ctl'] .
+      '</p></body></html>';
     exit;
   }
-} else { 
+} else {
   $ruta = 'inicio';
 }
 
 $controlador = $mapeoRutas[$ruta];
 // Ejecución del controlador asociado a la ruta
 
-if (method_exists($controlador['controller'],$controlador['action']))
-{ 
-  call_user_func(array(new $controlador['controller'], $controlador['action']),
-  (isset($param)?$param:''));
-} else { 
+if (method_exists($controlador['controller'], $controlador['action'])) {
+  call_user_func(
+    array(new $controlador['controller'], $controlador['action']),
+    (isset($param) ? $param : '')
+  );
+} else {
   header('Status: 404 Not Found');
   echo '<html><body><h1>Error 404: El controlador <i>' .
-       $controlador['controller'] .
-       '->' . $controlador['action'] .
-       '</i> no existe</h1></body></html>';
+    $controlador['controller'] .
+    '->' . $controlador['action'] .
+    '</i> no existe</h1></body></html>';
 }
